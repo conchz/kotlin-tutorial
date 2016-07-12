bf2dc0794eb129a10f02a8e9c7d42f1f9361431b
Baymax <zongzhi.bai@dianrong.com>
2016-07-12 16:36:03 +0800

    Build kotlin project by gradle.

---
+++ src/main/kotlin/org/lavenderx/HelloWorld.kt
@@ -0,0 +1,11 @@
+package org.lavenderx
+
+fun main(args: Array<String>) {
+    val s = "First"
+    println("Hello, World!")
+    println("""
+    $s
+    Second
+    Third
+    """.trimIndent())
+}
